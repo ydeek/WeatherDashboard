a47d788160964958ed2488461f7d421c3a1b1c4c
Yazan Deek <ydeek91@yahoo.com>
2020-09-15 21:10:42 -0400

    updated script and Readme.

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,17 @@
-
-let city = "";
-let url = "";
-let APIkey = "";
-let queryurl = "";
-let currenturl = "";
-let citiesDiv = document.getElementById("searched_cities_container");
-//start with empty array
-let cities = [];
+//declared variables
+var city = "";
+var url = "";
+var APIkey = "";
+var queryurl = "";
+var currenturl = "";
+var citiesDiv = document.getElementById("searched_cities_container");
+// started with an empty array 
+var cities = [];
 init();
 
 
-//run function to pull saved cities from local storage and fill array with it
 function init() {
-    let savedCities = JSON.parse(localStorage.getItem("cities"));
+    var savedCities = JSON.parse(localStorage.getItem("cities"));
 
     if (savedCities !== null) {
         cities = savedCities
@@ -21,23 +20,22 @@ function init() {
     renderButtons();
 }
 
-//sets localstorage item to cities array 
+
 function storeCities() {
     localStorage.setItem("cities", JSON.stringify(cities));
 }
 
 
-//render buttons for each element in cities array as a search history for user
 function renderButtons() {
     citiesDiv.innerHTML = "";
     if (cities == null) {
         return;
     }
-    let unique_cities = [...new Set(cities)];
-    for (let i = 0; i < unique_cities.length; i++) {
-        let cityName = unique_cities[i];
+    var unique_cities = [...new Set(cities)];
+    for (var i = 0; i < unique_cities.length; i++) {
+        var cityName = unique_cities[i];
 
-        let buttonEl = document.createElement("button");
+        var buttonEl = document.createElement("button");
         buttonEl.textContent = cityName;
         buttonEl.setAttribute("class", "listbtn");
 
@@ -45,7 +43,7 @@ function renderButtons() {
 
     }
 }
-//on click function for search history buttons
+//function for search history buttons
 
 $(".listbtn").on("click", function (event) {
     console.log("anybody home?")
@@ -58,17 +56,16 @@ $(".listbtn").on("click", function (event) {
 
 
 
-//on click function for main search bar
+//Function for main search 
 function searchClicker() {
     city = $("#searchTerm").val().trim()
     console.log(city)
-    //push the city user entered into cities array 
-    cities.push(city);
-    //make sure cities array.length is never more than 8 
+
+
     if (cities.length > 8) {
         cities.shift()
     }
-    //return from function early if form is blank
+
     if (city == "") {
         return;
     }
@@ -81,7 +78,7 @@ $("#searchBtn").on("click", function () {
     searchClicker()
 })
 
-//runs  API calls, one for current weather data and one for five-day forecast, then populates text areas
+//APIs calls for curent weather and other weather information. 
 const APIcalls = (city) => {
 
     APIkey = "e003e884a673c8994a9d786afd4dcdc8"
@@ -93,17 +90,18 @@ const APIcalls = (city) => {
             console.log(locationInfo)
 
             full_url = `https://api.openweathermap.org/data/2.5/onecall?&lat=${locationInfo.coord.lat}&lon=${locationInfo.coord.lon}&appid=${APIkey}`
+
+
             console.log(full_url)
             $("#name_of_city").text("Today's Weather in " + city);
             $.ajax({
                 method: "GET",
                 url: full_url,
             }).then(function (response) {
-                let day_number = 0;
+                var day_number = 0;
                 console.log(response)
-                //iterate through the 40 weather data sets
-                for (let i = 0; i < response.daily.length; i++) {
-                    console.log(response.daily[i]) //look at movie activity 6-10 (movie)
+                for (var i = 0; i < response.daily.length; i++) {
+                    console.log(response.daily[i])
 
                 }
             });
@@ -112,13 +110,13 @@ const APIcalls = (city) => {
 
 function getCurrentConditions(response) {
 
-    // get the temperature and convert to fahrenheit 
-    let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    // convert tempreture to F 
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
     tempF = Math.floor(tempF);
 
     $('#currentCity').empty();
 
-    // get and set the content 
+
     const card = $("<div>").addClass("card");
     const cardBody = $("<div>").addClass("card-body");
     const city = $("<h4>").addClass("card-title").text(response.name);
@@ -128,7 +126,7 @@ function getCurrentConditions(response) {
     const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
     const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
 
-    // add to page
+    // this for add to the page 
     city.append(cityDate, image)
     cardBody.append(city, temperature, humidity, wind);
     card.append(cardBody);
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# WeatherDashboard
+# Weather Dashboard
+
+Weather Dashboard is the sixth homework assignment for Penn LSP Coding Bootcamp. This application built with html, Javascript, jQuery, and using APIs. 
+In this weather dashboard users can search for any city to check current weather along with other weather information. The searched cities can be saved and listed under search bar. 
